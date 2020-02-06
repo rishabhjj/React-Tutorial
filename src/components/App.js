e1650e37db6d169505e3428443dddd2ffc001142
Rishabh Jain <rishabh.jain@MACPRO136.local>
2020-02-06 22:53:40 +0530

    Commit 2 - JSX

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,15 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>React Main Page</h1>
+        <h1 name ="Rishabh" className = "first">Jain</h1>
+        <h2 name htmlFor = "test">Boolean</h2>
+        <h2 name= "true">string</h2>
+        <h2 name = {false}> Boolean 2</h2>
+        <h2 name = {1}> Numeric</h2>
+        <div name = { 10 + 20 }> Operations</div>
+        <div>
+          <div>Testing</div>
+        </div>
       </div>
     ) 
   }
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'index.[contentHash].bundle.js'
+    filename: 'index.bundle.js'
   },
   module: {
     rules: [
