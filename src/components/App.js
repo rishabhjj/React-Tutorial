046a138975a02236b0c068faed1fcbd67670a9a3
Rishabh Jain <rishabh.jain@MACPRO136.local>
2020-02-12 12:35:12 +0530

    Pure Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 
-class App extends Component {
+class App extends PureComponent {
   constructor() {
     super()
     console.log('First Lifecycle Method - Constructor')
     this.state = {
-      title: 'Learning Lifecycle Methods'
+      score: 1
     }
   }
-  componentWillMount() {
-    setTimeout(() => {
-      this.setState({
-        title: 'tesing'
+  componentDidMount() {
+    console.log("component did mount")
+    setInterval(() => {
+      this.setState(() => {
+        return {score: 1}
       })
-    }, 10000);
-
+    }, 2000);
   }
-  // componentDidMount() {
-  //   console.log("component Did mount");
-  //   // this.setState({
-  //   //   title: 'Learned Component Did Mount'
-  //   // })
-  // }
-  // customEvent = () => {
-  //   this.setState({
-  //     title: 'Updating Title With Custom Click'
-  //   })
-  // }
-  // componentWillUpdate() {
-  //   // this.setState({
-  //   //   title: 'Component Will Update'
-  //   // })
-  // }
-  // shouldComponentUpdate(nextProps, nextstate) {
-  //   // console.log(nextProps, nextstate);
-  //   return true;
-  // }
-  // componentDidUpdate(previousProps, previousState) {
-  //   console.log('Component Did Update', this.state)
-  //   if (this.state.title !== previousState.title) {
-  //     this.setState({
-  //       title: 'Component Will Update'
-  //     })
-  //   }
-  // }
-  // componentWillUnmount() {
-  //   console.log('component will Unmount');
-  // }
-  // unmount = () => {
-  //   this.setState({
-  //     title: 'unmount'
-  //   })
-  // }
+
   render() {
     console.log("calling render")
-    console.log(this.state);
-    if (this.state.title === 'unmount') {
-      console.log("com")
-      return (<div/>);
-    }
     return (
       <div>
         <h1>React Main Page</h1>
-        {this.state.title}
-        <br />
-        <button onClick= {this.customEvent}>Change Title</button>
-        <button onClick = {this.unmount}> UnMount</button>
+        {this.state.score}
       </div>
     ) 
   }
